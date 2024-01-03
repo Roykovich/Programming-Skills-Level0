21f10bd6d4f962660fa7badaba5535f78b5d5f1d
Rubén Ortiz <runerd.menethil@gmail.com>
2024-01-03 16:41:39 -0300

    Add a little bit more of documentation

--- 1/README.md
+++ 1/README.md
@@ -7,3 +7,10 @@
 - The initial balance in the bank account is $2000.
 - The system must allow users to deposit, withdraw, view, and transfer money.
 - The system must display a menu for users to perform transactions.
+
+## Explanation
+
+### Tranfer
+
+In this secion I decided to only use an "account number" to validate where is the money going to be sent, taking inspiration in the CuentaRut from Banco Estado in Chile. But I'm aware that in most of the situations to do these kind of transactions the app are going to ask for name, ID, account number, account type and so on.
+
--- 1/banking.py
+++ 1/banking.py
@@ -41,12 +41,16 @@ def main():
         match action:
             case 'deposit':
                 try: 
+                    # First we ask for the amount, it should be an integer or it will raise a ValueError
                     amount = int(input('Please enter the amount: '))
+                    # Then we check if the amount is greater or equal to 0
                     if amount <= 0:
                         print('Invalid amount\n')
                         continue
+                    # If the amount is valid, we add it to the balance and print the new balance
                     balance += amount
                     print(f'You have successfully deposited {amount}. Your new balance is {balance}\n')
+                    # Finally we ask the user what he wants to do next
                     action = input('[deposit|withdraw|balance|transfer|logout]')
                     continue
                 except ValueError:
@@ -55,12 +59,15 @@ def main():
 
             case 'withdraw':
                 try:
+                    # First we ask for the amount, it should be an integer or it will raise a ValueError
                     amount = int(input('Please enter the amount: '))
+                    # Then we call the withdraw function, it will return the new balance or None if the amount is invalid
                     new_balance: int = withdraw(amount, balance)
-
+                    # If the amount is valid we update the balance and print the new balance
                     if new_balance is not None:
                         balance = new_balance
                         print(f'Your new balance is: {new_balance}\n')
+                        # Finally we ask the user what he wants to do next
                         action = input('[deposit|withdraw|balance|transfer|logout]')
                         continue
                 except ValueError:
@@ -68,37 +75,48 @@ def main():
                     continue
 
             case 'balance':
+                # We just print the balance and ask the user what he wants to do next
                 print(f'Your balance is: {balance}')
                 action = input('[deposit|withdraw|balance|transfer|logout]')
 
             case 'transfer':
                 try:
+                    # First we ask for the account number and the amount, they should be integers or it will raise a ValueError
                     account = int(input('Please enter the account number: '))
                     amount = int(input('Please enter the amount: '))
 
+                    # Then we call the transfer function, it will return the new balance or None if the account or the amount are invalid
                     new_balance: int = transfer(account, amount, balance)
 
+                    # If the amount is valid we update the balance and print the new balance
                     if new_balance is not None:
                         balance = new_balance
                         print(f'Your new balance is: {new_balance}\n')
+                        # Finally we ask the user what he wants to do next
                         action = input('[deposit|withdraw|balance|transfer|logout]')
                         continue
                     
+                    # If the account or the amount are invalid, we ask the user if he wants to try again
                     option = input('Want to try again? [y/n] ')
 
+                    # If the user wants to try again, we continue the loop
+                    # and ask for the account and the amount again
                     if option == 'y':
                         continue
-                        
+                    
+                    # If the user doesn't want to try again, we ask the user what he wants to do next
                     action = input('[deposit|withdraw|balance|transfer|logout]')
                 except ValueError:
                     print('Invalid input\n')
                     continue
 
             case 'logout':
+                # We just print a message and end the program
                 print('You have successfully logged out. Cya later!')
                 return
             
             case _:
+                # If the user types an invalid action, we ask the user what he wants to do next
                 print('INVALIDAD ACTION')
                 action = input('[deposit|withdraw|balance|transfer|logout]')
 
